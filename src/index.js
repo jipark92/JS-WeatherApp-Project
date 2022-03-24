b53eb2cda523e4572a8b216fa25ef6c6a068b1dd
ji <jiyongprk@gmail.com>
2022-03-23 21:23:57 -0400

    Celsius function working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const weatherModule = (()=>{
     const searchBar = document.querySelector('.search-location');
     const searchBtns = document.querySelector('.search-btn');
-    
+    const toggleCelFahBtns = document.querySelector('.toggle-fahrenheit-btn');
     const locationName = document.querySelector('.location-name')
     const currently = document.querySelector('.currently');
     const clouds = document.querySelector('.clouds');
@@ -10,32 +10,58 @@ const weatherModule = (()=>{
     const pressure = document.querySelector('.pressure');
     const minTemp = document.querySelector('.min-temp');
     const maxTemp = document.querySelector('.max-temp');
+    const icons = document.querySelectorAll('.icons');
 
     const getWeather = (location) =>{
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=88cfbde6ec14cc6b1ea870b1a0aaa9b5`, {mode: 'cors'})
         .then((response)=>{
-            return response.json()
+            return response.json();
         })
         .then((response)=>{
-            locationName.textContent = response.name;
-            currently.textContent = response.main.temp;
+
+            locationName.textContent = response.name
+            currently.textContent = Math.round(response.main.temp - 273) / 1 + "C";
             clouds.textContent = response.weather[0].description;
-            feelsLike.textContent = response.main.feels_like;
+            feelsLike.textContent = Math.round(response.main.feels_like - 273) / 1 + "C";
             humidity.textContent = response.main.humidity;
-            pressure.textContent = response.main.pressure;
-            minTemp.textContent = response.main.temp_min;
-            maxTemp.textContent = response.main.temp_max;
-            console.log('clicked')
+            pressure.textContent = response.main.pressure + "mb";
+            minTemp.textContent = Math.round(response.main.temp_min - 273) *  1 + "C";
+            maxTemp.textContent = Math.round(response.main.temp_max - 273) * 1 + "C";
+
+        })
+        .catch(()=>{
+            alert("no location found");
         })
     };
     
     const searchButton = () => {
         searchBtns.addEventListener('click',()=>{
             let locations = searchBar.value;
-            getWeather(locations);
+            if (searchBar.value === "" || searchBar === null){
+                return
+            } else {
+                getWeather(locations);
+                makeIconsVisible();
+
+            }
         })
-    }
+    };
     searchButton();
 
-    return {};
+    const makeIconsVisible = () => {
+        icons.forEach((icon)=>{
+            icon.style.visibility = "visible";
+        })
+    };
+
+    const toggleFahrenheit = (response) => {
+        toggleCelFahBtns.addEventListener('click',()=>{
+            currently.textContent = Math.round(response.main.temp - 273) * 1 
+            feelsLike.textContent = Math.round(response.main.feels_like - 273) * 1 
+            minTemp.textContent = Math.round(response.main.temp_min - 273) *  1  
+            maxTemp.textContent = Math.round(response.main.temp_max - 273) * 1 
+        })
+    }
+
+
 })();
